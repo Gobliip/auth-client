e4ff511cb99f9b9021063b7bf54052c681ac78d0
Leonel Samayoa <leonel.samayoa@xoom.com>
2015-07-15 15:43:12 -0600

    Extracted retrofit-cloud

--- src/main/java/com/gobliip/auth/client/config/AuthenticationClientConfiguration.java
+++ src/main/java/com/gobliip/auth/client/config/AuthenticationClientConfiguration.java
@@ -3,16 +3,20 @@ package com.gobliip.auth.client.config;
 import com.gobliip.auth.client.AuthenticationClient;
 import com.gobliip.auth.client.AuthenticationClientImpl;
 import com.gobliip.auth.client.retrofit.AuthenticationServiceResource;
-import com.gobliip.retrofit.oauth2.BasicAuthCredentialsStore;
-import com.gobliip.retrofit.oauth2.InMemoryBasicAuthCredentialsStore;
-import com.gobliip.retrofit.oauth2.OAuthBasicAuthInterceptor;
+import com.gobliip.retrofit.cloud.endpoint.RoundRobinEndpoint;
+import com.gobliip.retrofit.cloud.oauth2.basic.BasicAuthCredentialsStore;
+import com.gobliip.retrofit.cloud.oauth2.basic.InMemoryBasicAuthCredentialsStore;
+import com.gobliip.retrofit.cloud.oauth2.basic.OAuthBasicAuthInterceptor;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import retrofit.Endpoint;
+import retrofit.Endpoints;
 import retrofit.RestAdapter;
 import retrofit.converter.GsonConverter;
 
@@ -22,17 +26,26 @@ import retrofit.converter.GsonConverter;
 @Configuration
 public class AuthenticationClientConfiguration {
 
-    @Value("${gobliip.oauth2.clientId}")
+    @Value("${gobliip.authentication.clientId}")
     private String clientId;
 
-    @Value("${gobliip.oauth2.clientSecret}")
+    @Value("${gobliip.authentication.clientSecret}")
     private String clientSecret;
 
-    @Value("${gobliip.oauth2.endpoint}")
+    @Value("${gobliip.authentication.service-name:authentication")
+    private String serviceName;
+
+    @Value("${gobliip.authentication.endpoint:http://authentication.gobliip.:5021/}")
     private String endpoint;
 
     @Autowired(required = false)
-    private BasicAuthCredentialsStore credentialsStore;
+    private OAuthBasicAuthInterceptor oAuthBasicInterceptor;
+
+    @Autowired(required = false)
+    private BasicAuthCredentialsStore basicAuthCredentialsStore;
+
+    @Autowired(required = false)
+    private DiscoveryClient discoveryClient;
 
     @Bean
     public AuthenticationClient authenticationClient(){
@@ -48,7 +61,7 @@ public class AuthenticationClientConfiguration {
     public RestAdapter restAdapter(){
         final RestAdapter restAdapter = new RestAdapter.Builder()
                 .setConverter(new GsonConverter(gson()))
-                .setEndpoint(endpoint)
+                .setEndpoint(authenticationEndpoint())
                 .setRequestInterceptor(oAuthBasicInterceptor())
                 .build();
         return restAdapter;
@@ -64,13 +77,22 @@ public class AuthenticationClientConfiguration {
 
     @Bean
     public OAuthBasicAuthInterceptor oAuthBasicInterceptor(){
-        return new OAuthBasicAuthInterceptor(credentialsStore());
+        if (oAuthBasicInterceptor != null) return oAuthBasicInterceptor;
+        return new OAuthBasicAuthInterceptor(basicAuthCredentialsStore());
     }
 
     @Bean
-    public BasicAuthCredentialsStore credentialsStore(){
-        if(credentialsStore != null) return credentialsStore;
-        return new InMemoryBasicAuthCredentialsStore(clientId, clientSecret);
+    public BasicAuthCredentialsStore basicAuthCredentialsStore(){
+        if(basicAuthCredentialsStore != null) return basicAuthCredentialsStore;
+        return new InMemoryBasicAuthCredentialsStore(clientId,  clientSecret);
+    }
+
+    @Bean
+    public Endpoint authenticationEndpoint(){
+        if(discoveryClient != null) {
+            return new RoundRobinEndpoint(discoveryClient, serviceName);
+        }
+        return Endpoints.newFixedEndpoint(endpoint);
     }
 
 }
